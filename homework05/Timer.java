a785085f4af55b78d57a2da1de4942ff044c45a7
KPMcInerney <kevin.p.mcinerney@gmail.com>
2018-03-19 13:14:04 -0700

    Create Timer.java

---
+++ homework05/Timer.java
@@ -0,0 +1,105 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Timer.java
+ *  Purpose       :  Provides a class defining methods for the ClockSolver class
+ *  @author       :  B.J. Johnson
+ *  Date written  :  2017-02-28
+ *  Description   :  This class provides a bunch of methods which may be useful for the ClockSolver class
+ *                   for Homework 4, part 1.  Includes the following:
+ *
+ *  Notes         :  None right now.  I'll add some as they occur.
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class Timer {
+  /**
+   *  Class field definintions go here
+   */
+   private static final double DEFAULT_TIME_SLICE_IN_SECONDS = 1.0;
+   private static final double INVALID_ARGUMENT_VALUE = -1.0;
+   private double totalSeconds, timeSlice;
+  /**
+   *  Constructor goes here
+   */
+   public Timer( String args[] ) { //initializes variables in contructor
+      totalSeconds = 0;
+      if ( ((args.length % 4) + 1) == 0 ){ //creates timeSlice if ones not given
+         timeSlice = validateTimeSliceArg( args[0] );
+      } else {
+         timeSlice = validateTimeSliceArg( "" );
+      }
+   }
+
+  /**
+   *  Methods go here
+   *
+   *  Method to calculate the next tick from the time increment
+   *  @return double-precision value of the current clock tick
+   */
+   public double tick() {
+      totalSeconds += timeSlice;
+      return totalSeconds;
+   }
+
+  /**
+   *  Method to validate the optional time slice argument
+   *  @param  argValue  String from the main programs args[1] input
+   *  @return double-precision value of the argument or -1.0 if invalid
+   *  note: if the main program determines there IS no optional argument supplied,
+   *         I have elected to have it substitute the string "60.0" and call this
+   *         method anyhow.  That makes the main program code more uniform, but
+   *         this is a DESIGN DECISION, not a requirement!
+   *  note: remember that the time slice, if it is small will cause the simulation
+   *         to take a VERY LONG TIME to complete!
+   */
+   public double validateTimeSliceArg( String argValue ) throws NumberFormatException {
+      if ( argValue.equals(null) || argValue.equals("") ){ //if no value given return the default
+         timeSlice = DEFAULT_TIME_SLICE_IN_SECONDS;
+         return timeSlice;
+      }
+      double given = Double.parseDouble(argValue);
+      if ( given > 1 ){ //if given value is valid, return it
+         return given;
+      } else {
+         throw new NumberFormatException(); //if invalid value given throw error
+      }
+   }
+
+  /**
+   *  Method to fetch the total number of seconds
+   *   we can use this to tell when 12 hours have elapsed
+   *  @return double-precision value the total seconds private variable
+   */
+   public double getTotalSeconds() {
+      return totalSeconds; //returns total seconds
+   }
+
+  /**
+   *  Method to return a String representation of this clock
+   *  @return String value of the current clock
+   */
+   public String toString() {
+      return " TimeSlice arg: " + timeSlice; //returns representation of clock as a string
+   }
+
+  /**
+   *  The main program starts here
+   *  remember the constraints from the project description
+   *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
+   *  be sure to make LOTS of tests!!
+   *  remember you are trying to BREAK your code, not just prove it works!
+   */
+   public static void main( String args[] ) {
+      System.out.println( "\nTIMER CLASS TESTER PROGRAM\n" +
+                          "--------------------------\n" );
+      System.out.println( "  Creating a new timer: " );
+      Timer timer = new Timer( args );
+      System.out.println( "    New timer created: " + timer.toString() );
+   }
+}
